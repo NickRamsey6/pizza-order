51c3b072ecdaf46422a417814c43dc224e5f904b
Nick Ramsey <nramseysc@gmail.com>
2019-04-01 09:54:22 -0700

    add seperate displays for toppings and size and return a price based on size

--- js/scripts.js
+++ js/scripts.js
@@ -6,37 +6,41 @@ function Order (toppings, size) {
   this.size = size;
 }
 
-var toppingsPrice = function toppingsPrice() {
-  var toppings = [];
-  if(toppings.lenght === 1) {
-    toppingsPrice = 1
-  }
-  else if (toppings.length === 2) {
-    toppingsPrice = 2
-  }
-  else if (toppings.lenght ===3) {
-    toppingsPrice = 3
-  }
-  else {
-    toppingsPrice = 4
-  }
+Order.prototype.newOrder = function(toppings, size) {
+  return this.toppings + this.size
 }
 
-var sizePrice = function sizePrice() {
-  if(pizzaSizeOrder === "Small") {
-    sizePrice = 1
-  }
-  else if (pizzaSizeOrder === "Medium") {
-    sizePrice = 2
-  }
-  else {
-    sizePrice = 3
-  }
-}
-
-var totalPrice = function totalPrice(toppingsPrice, sizePrice) {
-  return toppingsPrice * sizePrice;
-};
+// var toppingsPrice = function toppingsPrice() {
+//   var toppings = [];
+//   if(toppings.length === 1) {
+//     toppingsPrice = 1
+//   }
+//   else if (toppings.length === 2) {
+//     toppingsPrice = 2
+//   }
+//   else if (toppings.length ===3) {
+//     toppingsPrice = 3
+//   }
+//   else {
+//     toppingsPrice = 4
+//   }
+// }
+//
+// var sizePrice = function sizePrice() {
+//   if(pizzaSizeOrder === "Small") {
+//     sizePrice = 1
+//   }
+//   else if (pizzaSizeOrder === "Medium") {
+//     sizePrice = 2
+//   }
+//   else {
+//     sizePrice = 3
+//   }
+// }
+//
+// var totalPrice = function totalPrice(toppingsPrice, sizePrice) {
+//   return toppingsPrice * sizePrice;
+// };
 
 
 
@@ -44,20 +48,30 @@ var totalPrice = function totalPrice(toppingsPrice, sizePrice) {
 $(document).ready(function(){
   $("form#pizza-order").submit(function(event){
     event.preventDefault();
-    $("#your-order").show();
+    // $("#your-order").show();
     var toppings = [];
     $("input:checkbox[name=pizza-topping]:checked").each(function(){
       var pizzaToppingOrder = $(this).val();
       toppings.push(pizzaToppingOrder);
-      alert(toppings.length);
-      var pizzaSizeOrder = $("select#size").val();
-      $("#your-order").append(pizzaToppingOrder + "<br>" + pizzaSizeOrder);
+      $("#your-toppings").append(pizzaToppingOrder + "<br>");
+      $("#your-toppings").show();
     });
+    var pizzaSizeOrder = $("select#size").val();
+    $("#your-size").append(pizzaSizeOrder + "<br>");
+    $("#your-size").show();
+    // var totalPrice = function totalPrice(toppingsPrice, sizePrice) {
+    //   return toppingsPrice * sizePrice;
+    //   $("#your-price").append(totalPrice + "<br>");
+    //   $("#your-price").show();
+    // };
+    var price = 10;
+    if (pizzaSizeOrder === "Medium") {
+      price +=5;
+    } else if (pizzaSizeOrder === "Large") {
+      price +=10;
+    }
+    $("#your-price").append(price + "<br>");
     $("#your-price").show();
-    var totalPrice = function totalPrice(toppingsPrice, sizePrice) {
-      return toppingsPrice * sizePrice;
-      $("your-price").append(totalPrice + "<br>");
-    };
   });
     // $('#transportation_survey').hide();
 });
